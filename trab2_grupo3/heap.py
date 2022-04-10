16de18532295872e3e089cb7ca75504a6212d9f6
Bel Cogo <bel.cogo@cwi.com.br>
2022-04-09 22:04:28 -0300

    fix: ajusta método de inserção

--- trab2_grupo3/heap.py
+++ trab2_grupo3/heap.py
@@ -2,12 +2,15 @@ class BinaryHeap:
   stringTree = ""
   # void
   def setInitialConfigs(self):
-    self.n = 0
     self.pq = [None] * 10
+    self.n = 0
 
   # void
   def __init__(self):
     self.setInitialConfigs()
+  
+  def PQ(self):
+    self.clear()
 
   # returns boolean
   def isEmpty(self):
@@ -29,18 +32,18 @@ class BinaryHeap:
     self.pq = temp
 
   # void
-  def bottomUpHeapfy(self, i):
-    index = int(i)
-    parent = int((index - 1) / 2)
-    if index > 0 and index <= self.n and self.compareTo(parent, index):
-      self.exchange(index, parent)
-      self.bottomUpHeapfy(parent)
+  def bottomUpHeapify(self, i):
+    parent = int((i - 1) / 2)
+    print(parent)
+    if i > 0 and self.compareTo(parent, i):
+      self.exchange(i, parent)
+      self.bottomUpHeapify(parent)
   
   # void
   def topDownHeapify(self, i):
     index = int(i)
     temp = 2 * index + 1
-    if temp + 1 < self.n and self.compareTo(temp, temp + 1):
+    if (temp + 1) < self.n and self.compareTo(temp, temp + 1):
       temp = temp + 1
     if temp < self.n and self.compareTo(index, temp): 
       self.exchange(index, temp)
@@ -50,13 +53,21 @@ class BinaryHeap:
   def exchange(self, i, j):
     indexI = int(i)
     indexJ = int(j)
-    swap = int(self.pq[indexI])
+    swap = self.pq[indexI]
     self.pq[indexI] = self.pq[indexJ]
     self.pq[indexJ] = swap
 
-  # returns boolean
+  # returns int
+  def baseCompareTo(self, i, j):
+    if (i is not None and j is None) or i > j: return 1
+    if (i is None and j is not None) or i < j: return -1
+    return 0
+  
+  # returns bool
   def compareTo(self, i, j):
-    return int(i) < int(j)
+    I = self.pq[int(i)]
+    J = self.pq[int(j)]
+    return self.baseCompareTo(I, J) > 0
 
   # void
   def increase(self, i, key):
@@ -66,6 +77,7 @@ class BinaryHeap:
       return
     if self.compareTo(key, self.pq[index]) < 0:
       print("Key is less than the key in the priority queue!")
+      return
     self.pq[index] = key
 
   # void
@@ -76,29 +88,30 @@ class BinaryHeap:
       return
     if self.compareTo(key, self.pq[index]) > 0:
       print("Key is less than the key in the priority queue!")
+      return
     self.pq[index] = key
 
   # returns int
-  def key(self, i):
-    index = int(i)
+  def key(self):
     if self.isEmpty():
       print("Priority queue is empty")
-    return self.pq[index]
+    return self.pq[0]
 
   # void
   def insert(self, key):
     if self.n == len(self.pq):
       self.resize()
     self.pq[self.n] = key
+    self.bottomUpHeapify(self.n)
     self.n = self.n + 1
-    self.bottomUpHeapfy(self.n + 1)
 
   # returns int
   def extract(self):
     if self.isEmpty():
       print("Priority queue is empty")
     key = self.pq[0]
-    self.exchange(0, self.n - 1)
+    self.n = self.n - 1
+    self.exchange(0, self.n)
     self.topDownHeapify(0)
     self.pq[self.n] = None
     return key
@@ -115,32 +128,32 @@ class BinaryHeap:
         return b + "]"
       b = b + ", "
 
-  # void
-  def callPrintTree(self):
-    if self.n > 2: 
-      self.printTree(2, True, "")
+  # # void
+  # def callPrintTree(self):
+  #   if self.n > 2: 
+  #     self.printTree(2, True, "")
 
-    self.stringTree = (str(self.pq[0]) + "\n")
+  #   self.stringTree = (str(self.pq[0]) + "\n")
 
-    if self.n > 1:
-      self.printTree(1, False, "")
+  #   if self.n > 1:
+  #     self.printTree(1, False, "")
 
-    return self.stringTree
+  #   return self.stringTree
 
-  # void
-  def printTree(self, index, isRight, indent):
-    rightChild = index * 2 + 2
+  # # void
+  # def printTree(self, index, isRight, indent):
+  #   rightChild = index * 2 + 2
   
-    if rightChild < self.n:
-      rightIndent = indent + "         " if isRight else " |        "
-      self.printTree(rightChild, True, rightIndent)
+  #   if rightChild < self.n:
+  #     rightIndent = indent + "         " if isRight else " |        "
+  #     self.printTree(rightChild, True, rightIndent)
     
-    self.stringTree = self.stringTree + indent + ("/" if isRight else "\\") + str(self.pq[index]) + "\n"
-    leftChild = index * 2 + 1
+  #   self.stringTree = self.stringTree + indent + ("/" if isRight else "\\") + str(self.pq[index]) + "\n"
+  #   leftChild = index * 2 + 1
     
-    if leftChild < self.n:
-      leftIndent = indent + " |        " if isRight else "          "
-      self.printTree(leftChild, False, leftIndent)
+  #   if leftChild < self.n:
+  #     leftIndent = indent + " |        " if isRight else "          "
+  #     self.printTree(leftChild, False, leftIndent)
     
 
 
--- trab2_grupo3/main.py
+++ trab2_grupo3/main.py
@@ -4,9 +4,17 @@ global heap
 
 heap = BinaryHeap()
 
+# print(heap.toString())
+
 heap.insert(int(10))
+heap.insert(int(7))
 heap.insert(int(20))
 heap.insert(int(30))
 heap.insert(int(40))
+heap.insert(int(6))
+heap.insert(int(9))
 
-heap.callPrintTree()
+
+
+print(heap.toString())
+print(heap.key())
